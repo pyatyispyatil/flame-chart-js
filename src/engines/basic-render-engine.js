3e6cf2f53fc85f3cf2aa1fc94ef4ec768a43fff0
Amit Turner <amit.turner@granulate.io>
2021-11-01 18:48:41 +0200

    Update basic-render-engine.js

--- src/engines/basic-render-engine.js
+++ src/engines/basic-render-engine.js
@@ -264,7 +264,6 @@ export class BasicRenderEngine extends EventEmitter {
     }
 
     resetView() {
-        console.log('reset!')
         this.setZoom(this.getInitialZoom());
         this.setPositionX(this.min);
     }

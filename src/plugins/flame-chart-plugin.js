973f5493d378757bd66e0e878fa54a2de301ba5c
Amit Turner <amit.turner@granulate.io>
2021-11-17 11:14:21 +0200

    testing y position

--- src/plugins/flame-chart-plugin.js
+++ src/plugins/flame-chart-plugin.js
@@ -67,8 +67,7 @@ export default class FlameChartPlugin extends UIPlugin {
     reset() {
         this.colors = {};
         this.lastRandomColor = DEFAULT_COLOR;
-        //disabled to prevent selecting a node go to top
-        //this.positionY = 0;
+        this.positionY = this.positionY || 0;
         this.selectedRegion = null;
     }
 

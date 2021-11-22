2b28936549a89c5f623730f5c94f42f0cf0f19f2
Amit Turner <amit.turner@granulate.io>
2021-11-22 12:45:18 +0200

    Update tree-clusters.js

--- src/plugins/utils/tree-clusters.js
+++ src/plugins/utils/tree-clusters.js
@@ -138,6 +138,7 @@ export const clusterizeFlatTree = (metaClusterizedFlatTree, zoom, start, end, st
         .map((nodes) => {
             const node = nodes[0];
             const duration = calcClusterDuration(nodes);
+            console.log(node.matched)
             return {
                 start: node.start,
                 end: node.start + duration,

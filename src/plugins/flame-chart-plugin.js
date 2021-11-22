cb229cf4451b831662f5e8ef59cfbe10d09c840e
Amit Turner <amit.turner@granulate.io>
2021-11-22 18:55:56 +0200

    test hover

--- src/plugins/flame-chart-plugin.js
+++ src/plugins/flame-chart-plugin.js
@@ -285,6 +285,12 @@ export default class FlameChartPlugin extends UIPlugin {
             this.renderEngine.addStrokeToRenderQueue('black', x, y, w, this.renderEngine.blockHeight);
         }
 
+        if (this.hoveredRegion && this.hoveredRegion.type === 'node'){
+            const { hoveredStart, hoveredDuration, hoveredLevel } = this.hoveredRegion.data;
+            const { x, y, w } = this.calcRect(hoveredStart, hoveredDuration, hoveredLevel);
+            this.renderEngine.addStrokeToRenderQueue('red', x, y, w, this.renderEngine.blockHeight);
+        }
+
         clearTimeout(this.renderChartTimeout);
 
         this.renderChartTimeout = setTimeout(() => {

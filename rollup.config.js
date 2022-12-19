5460f42cc21bac4531c4f1a8dbc3ae11728ed808
James Burrow <jaburrow@gmail.com>
2022-12-19 11:37:17 +0000

    revert formatting

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
-import babel from 'rollup-plugin-babel';
-import builtins from 'rollup-plugin-node-builtins';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import html from '@rollup/plugin-html';
-import cleaner from 'rollup-plugin-cleaner';
-import { template } from './example/src/template.js';
-import typescript from '@rollup/plugin-typescript';
+import babel from 'rollup-plugin-babel'
+import builtins from 'rollup-plugin-node-builtins'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import html from '@rollup/plugin-html'
+import cleaner from 'rollup-plugin-cleaner'
+import { template } from './example/src/template.js'
+import typescript from '@rollup/plugin-typescript'
 
 export default {
     input: './example/src/index.js',
@@ -14,25 +14,27 @@ export default {
         dir: './example/dist',
         entryFileNames: 'main-[hash].js',
         format: 'iife',
-        name: 'bundle',
+        name: 'bundle'
     },
     plugins: [
-        typescript({ compilerOptions: { outDir: './example/dist' } }),        
+        typescript({ compilerOptions: { outDir: './example/dist' } }),
         resolve({
             browser: true,
-            preferBuiltins: true,
+            preferBuiltins: true
         }),
         commonjs(),
         builtins(),
         babel({
-            exclude: 'node_modules/**',
+            exclude: 'node_modules/**'
         }),
         json(),
         html({
             template,
         }),
         cleaner({
-            targets: ['./example/dist'],
-        }),      
-    ],
-};
+            targets: [
+                './example/dist'
+            ]
+        })
+    ]
+}
